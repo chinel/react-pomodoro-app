14734b44ef536a8b6a4485e17055102eced291df
nelo <nelokwaz@gmail.com>
2020-12-24 14:28:40 +0100

    stored the time in the state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft - minutes * 60;
   return (
     <div className="app">
       <h2>Pomodoro!</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
